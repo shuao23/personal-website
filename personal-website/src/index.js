a5782eb8d1a147a7398b08651493a597b8e5f072
shuao23 <shuao23@gmail.com>
2018-08-11 14:07:38 -1000

    installed firebase to app

--- personal-website/src/index.js
+++ personal-website/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
+
+
+var config = {
+  apiKey: "AIzaSyB5dUqtrT5Uz1rmx98SfrBnDUcyUkYgc70",
+  authDomain: "personal-website-121d7.firebaseapp.com",
+  databaseURL: "https://personal-website-121d7.firebaseio.com",
+  projectId: "personal-website-121d7",
+  storageBucket: "personal-website-121d7.appspot.com",
+  messagingSenderId: "439629536709"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
