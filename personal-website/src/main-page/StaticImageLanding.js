062b3994d2592b28a561bc089cb95518dd40a8fa
shuao23 <shuao23@gmail.com>
2018-08-15 08:19:41 -1000

    Fixed bugs

--- personal-website/src/main-page/StaticImageLanding.js
+++ personal-website/src/main-page/StaticImageLanding.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './StaticImageLanding.css';
+
 
 const StaticImageLanding = ({ src, children }) => {
   return (
-    <div className="StaticImageLanding" style={{ backgroundImage: "url(${ src });" }}>
+    <div className="StaticImageLanding" style={{ backgroundImage: `url(${src} )` }}>
       {children}
     </div >
   );
